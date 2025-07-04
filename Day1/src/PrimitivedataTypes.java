d6196ec26977bc7a60f4e2879c5ae58236ecab93
ADMIN <ADMIN@DESKTOP-1KKHH9J>
2025-07-03 23:52:21 -0700

    DAY!

---
+++ Day1/src/PrimitivedataTypes.java
@@ -0,0 +1,27 @@
+
+public class PrimitivedataTypes {
+
+
+	public static void main(String[] args) {
+		
+		//byte takes 1 byte
+		//1 byte = 8 bits     // 2^16
+		//256/2  
+		//128
+		
+		byte byteMax=127;
+		byte byteMin=-128;	
+		
+		System.out.println("Min range of byte is : "+ byteMin +
+				"  Max range of byte is "+ byteMax);
+		
+		//short ---- 2 byte 
+		short shortMax=  32767;
+		short shortMin=	-32768;	
+		
+		System.out.println("Min range of short is : "+ shortMin +
+				"  Max range of short is "+ shortMax);
+		
+	}
+
+}
---
+++ Day1/src/firstprogram.java
@@ -0,0 +1,10 @@
+
+public class firstprogram {
+
+	public class FirstProgram {
+
+		public static void main(String[] args) {
+			
+			System.out.println("..........Hello Friends.............");
+		}
+	}}
